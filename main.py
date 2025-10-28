6a5e033533aa9f0abd97cc6140f07d864395923d
SucukEray <kepcukmahmt@gmail.com>
2025-10-28 15:33:25 +0300

    Started writing the program

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+operatorList = []
+numberList = []
+
+operationAmount = int(input("Please enter the amount of operations: "))
+
+number = 0
+ordinalNumber = 0
+
+if abs(number) % 10 == 1 and abs(number) > 3:
+    ordinalNumber = f"{number}th"
+
+elif abs(number) % 10 == 1 and abs(number) <= 3:
+    ordinalNumber = f"{number}st"
+
+elif abs(number) % 10 == 2 and abs(number) <= 3:
+    ordinalNumber = f"{number}nd"
+
+elif abs(number) % 10 == 3 and abs(number) <= 3:
+    ordinalNumber = f"{number}rd"
+
+for number in range(operationAmount):
+    numberInput = int(input(f"What will your {ordinalNumber} number be?: "))
 
